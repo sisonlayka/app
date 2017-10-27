d865935c4abbb537ac5b4b2ffa400ddc52926d23
sisonlayka <sisonlayka@gmail.com>
2017-10-27 10:01:38 -0700

    update

---
+++ diary;/www/js/index.js
@@ -0,0 +1,28 @@
+function s(){
+    window.localStorage.setItem('save',document.getElementById('txtarea').value);
+}
+function v(){
+    document.getElementById('txtarea').value=window.localStorage.getItem('save');
+}
+
+var app = {
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
